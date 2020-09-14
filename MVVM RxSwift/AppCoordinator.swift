c19cf7dd7a81e63e5a48b4d670417ba31d6cdc3d
Fazle Rabbi Linkon <fazle.linkon@dreamonline.co.jp>
2020-09-14 16:17:32 +0600

    Added App Coordinator

---
+++ MVVM RxSwift/AppCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  AppCoordinator.swift
+//  MVVM RxSwift
+//
+//  Created by Fazle Rabbi Linkon on 14/9/20.
+//  Copyright © 2020 Fazle Rabbi Linkon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppCoordinator {
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start () {
+        let viewController = ViewController.instantiate()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+    
+}
--- MVVM RxSwift/ViewController.swift
+++ MVVM RxSwift/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    static func instantiate() -> ViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let viewController = storyboard.instantiateInitialViewController() as! ViewController
+        return viewController
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        view.backgroundColor = .red
     }
 
 
